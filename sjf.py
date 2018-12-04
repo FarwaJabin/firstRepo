b13e8a6edc0e701237e6d9c1d42bb0ccd9eb2358
Farwa Jabin <44096834+FarwaJabin@users.noreply.github.com>
2018-12-04 23:48:28 +0500

    Add files via upload

---
+++ sjf.py
@@ -0,0 +1,54 @@
+noOfprocesses=input("Enter number of Processes:")
+index=0
+n=0
+i=1
+burstTime=[]
+arrivalTime=[]
+startTime=[]
+finishTime=[]
+waitingTime=[]
+turnaroundTime=[]
+
+for index in range (int(noOfprocesses)):
+  print("Enter Arrival Time for process",i,":")
+  n=(input())
+  arrivalTime.append(n)
+  i=i+1
+i=1
+for index in range (int(noOfprocesses)):
+  print("Enter Burst Time for Process",i,":")
+  n=(input())
+  burstTime.append(n)
+  i=i+1
+
+i=0
+for index in range (int(noOfprocesses)):
+  burstTime.sort()
+  print(burstTime[i])
+  i=i+1
+
+for index in range (int(noOfprocesses)):
+  turnaroundTime.append(0)
+  finishTime.append(0)
+  waitingTime.append(0)
+
+print("Arrival Time\t Burst Time\t Waiting Time\t Turnaround Time")
+
+temp=int(arrivalTime[0])
+startTime=int(arrivalTime[0])
+for index in range (int(noOfprocesses)):
+  waitingTime[index]=startTime-int(arrivalTime[index])
+  temp=temp+int(burstTime[index])
+  finishTime[index]=temp
+  startTime=temp
+  turnaroundTime[index]=finishTime[index]-int(arrivalTime[index])
+  print(" ",arrivalTime[index],' \t\t ',burstTime[index],'\t\t ',waitingTime[index],'\t\t ',turnaroundTime[index])
+sumT=0 
+sumW=0
+for index in range(int(noOfprocesses)):
+   sumT=sumT+int(turnaroundTime[index])
+   sumW=sumW+int(waitingTime[index])
+averageT=sumT/int(noOfprocesses)
+averageW=sumW/int(noOfprocesses)
+print("Average Turnaround Time = ",averageT)
+print("Average Waiting Time = ",averageW)
